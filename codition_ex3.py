884b9a01c5dc47623ac7897826e78124dccab82d
senarchi4@gmail.com <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>
2023-03-15 20:04:22 +0530

    test1

---
+++ codition_ex3.py
@@ -0,0 +1,7 @@
+name=input('Enter your name please ')
+if len(name)<3:
+    print('Name must be at least 3 characters')
+elif len(name)>50:
+    print('Name can  be a maximum of 50 characters')
+else:
+    print('Name looks good')    
---
+++ condition.py
@@ -0,0 +1,11 @@
+price=1000000
+good_credit=True
+
+if good_credit:
+    price1=0.1*price
+    
+else:
+    price1=0.2*price
+   
+
+print(f'Down Payment {price1}')    
